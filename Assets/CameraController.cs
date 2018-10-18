a31663d7ffe6d758d0078c9a50773726f9287276
ferriperello - PC gran <ferriperello@gmail.com>
2018-10-18 19:58:20 +0200

    Primera part cubs

---
+++ Assets/CameraController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour {
+
+    float mainSpeed = 30.0f; //regular speed
+    float shiftAdd = 50.0f; //multiplied by how long shift is held.  Basically running
+    float maxShift = 100.0f; //Maximum speed when holdin gshift
+    float camSens = 0.25f; //How sensitive it with mouse
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    private float totalRun = 1.0f;
+    private float up_orientation = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, 0, 0.5f);
+            up_orientation += 0.5f;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 0, -0.5f);
+            up_orientation -= 0.5f;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * 2.5f, -Input.GetAxis("Mouse X") * 2.5f, 0));
+            float X = transform.rotation.eulerAngles.x;
+            float Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, up_orientation);
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.Rotate(0, 0, -(up_orientation));
+            up_orientation = 0.0f;
+        }
+
+        float f = 0.0f;
+        Vector3 p = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            p = p * totalRun * shiftAdd;
+            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+        }
+
+        p = p * Time.deltaTime;
+        transform.Translate(p);
+    }
+
+    private Vector3 GetBaseInput()
+    {
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        //Q --> angle cap a esq.
+
+        //E --> angle cap a dre.
+
+        //Z --> Y down
+        if (Input.GetKey(KeyCode.Z)){
+            p_Velocity += new Vector3(0, -1, 0);
+        }
+        //X --> Y up
+        if (Input.GetKey(KeyCode.X))
+        {
+            p_Velocity += new Vector3(0, 1, 0);
+        }
+        return p_Velocity;
+    }
+}
---
+++ Assets/CubeManaging.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManaging : MonoBehaviour {
+    public GameObject[] cubes;
+    public float[] volumes;
+    public int maxarray;
+
+    private bool creating;
+    private Vector3 minx;
+    private Vector3 maxx;
+    private Vector3 height;
+
+    public GameObject cube;
+    public Camera camera;
+
+    Ray ray;
+    RaycastHit hit;
+
+    // Use this for initialization
+    void Start () {
+        creating = false;
+        minx = new Vector3(0, 0, 0);
+        maxx = new Vector3(0, 0, 0);
+        cubes = new GameObject[10];
+        maxarray = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            creating = false;
+        }
+
+        if (Input.GetMouseButtonDown(0) & !creating)
+        {
+            //Vector3 spawnPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            //Debug.Log(spawnPosition.ToString());
+            //GameObject newCube = Instantiate(cube, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
+            Debug.Log("NOT");
+            this.transform.position = camera.ScreenToWorldPoint(Input.mousePosition);
+            ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out hit))
+            {
+                minx = hit.point;
+                //Debug.Log(minx.ToString());
+                creating = true;
+                //GameObject newCube = Instantiate(cube, new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z), Quaternion.identity);
+            }
+            creating = true;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) & Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("CREATING");
+            Create();
+        }
+    }
+
+    private void Create() {
+        if (Input.GetMouseButtonDown(0) & creating & maxx.Equals(new Vector3(0, 0, 0)))
+        {
+            Debug.Log("CREAT");
+            ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                maxx = hit.point;
+                creating = false;
+                //GameObject newCube = Instantiate(cube, new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z), Quaternion.identity);
+            }
+            Debug.Log("MINX");
+            Debug.Log(minx.ToString());
+            Debug.Log("MAXX");
+            Debug.Log(maxx.ToString());
+            GameObject newCube = Instantiate(cube, new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z), Quaternion.identity);
+            newCube.transform.position = new Vector3(minx.x+Mathf.Abs(minx.x-maxx.x), minx.y + Mathf.Abs(minx.y - maxx.y), minx.z + Mathf.Abs(minx.z - maxx.z));
+            newCube.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+            cubes[maxarray] = newCube;
+            maxarray += 1;
+        }
+        maxx = new Vector3(0, 0, 0);
+    }
+}
