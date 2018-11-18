b77d9b4aa1c47e0884f9f1257cffd56a306bd4b9
ferriperello - PC gran <ferriperello@gmail.com>
2018-11-19 00:49:44 +0100

    loader ben posat "de moment"

---
+++ Assets/ObjectLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AsImpL;
+
+public class ObjectLoader : MonoBehaviour {
+
+    public GameObject loadedObject;
+    public ImportOptions importOptions = new ImportOptions();
+    private ObjectImporter objImporter;
+
+    // Use this for initialization
+    void Start () {
+        string filePath = Menu_Manager.GetFilePath();
+        Debug.Log("LOADER");
+        Debug.Log(filePath);
+        objImporter = loadedObject.AddComponent<ObjectImporter>();
+        importOptions.buildColliders = true;
+
+        objImporter.ImportModelAsync("test", filePath, null, importOptions);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
