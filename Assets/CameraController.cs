f1e71281d20707ba5636b4d577a680e20aa808ef
ferriperello - PC gran <ferriperello@gmail.com>
2018-12-16 20:58:32 +0100

    Expansio Done

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -21,11 +21,12 @@ public class CameraController : MonoBehaviour {
     public bool[] findedTriangles;
     public int[] nts;
     public int[] ots;
+    public Vector3[] ovs;
     public int nVertex;
     public int nTriangles;
-    int lastIndexnts;
 
     public Dictionary<int, List<int>> neighboursinfo = new Dictionary<int, List<int>>();
+    public Dictionary<Vector3, List<int>> vertexinfo = new Dictionary<Vector3, List<int>>();
 
     public GameObject prefab;
 
@@ -42,6 +43,7 @@ public class CameraController : MonoBehaviour {
 
         //amb el objecte ja carregat
         ots = loadedObj.GetComponentInChildren<MeshFilter>().mesh.triangles;
+        ovs = loadedObj.GetComponentInChildren<MeshFilter>().mesh.vertices;
         nVertex = ots.Length;
         nTriangles = nVertex/3;
         findedTriangles = new bool[nTriangles];
@@ -55,135 +57,6 @@ public class CameraController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.T))
-        {
-            int loop = 100000;
-
-            Debug.Log("TESTING WITH " + loop + " rays");
-            var chrono = System.Diagnostics.Stopwatch.StartNew();
-            double total = 0;
-            double lasthitround = 1;
-            double minrounds = loop * 0.1;
-            Debug.Log("minrounds : " + minrounds);
-
-            Debug.Log(CubeManaging.GetTotalVolume());
-            //fer el loop
-            for (int i = 0; i < loop; i++)
-            {
-                if (!(lasthitround % minrounds == 0))
-                {
-                    if (i % 1000 == 0) Debug.Log("Iteració : " + i);
-                    //Vector3 randomXY = Random.rotation.eulerAngles;
-                    Vector3 randomXY = Random.insideUnitSphere;
-                    //Debug.Log(randomXY);
-                    //Debug.Log(sphere.transform.position);
-                    //Ray ray = new Ray(sphere.transform.position, new Vector3(randomXY.x, -1, randomXY.z));
-                    float randvolume = Random.Range(0.0f, CubeManaging.GetTotalVolume());
-                    ArrayList volumes = CubeManaging.GetVolumesArray();
-                    Debug.Log("randV"+randvolume);
-                    int k = 0;
-                    float volsdescarted = 0;
-                    Debug.Log("vol de " + k + ":" + volumes[k]);
-                    while ((volsdescarted +(float)volumes[k]) < randvolume)
-                    {
-                        Debug.Log("entro");
-                        volsdescarted += (float)volumes[k];
-                        k++;
-                        Debug.Log("vol de " + k + ":" + volumes[k]);
-                        Debug.Log("totalVuntilnow" + volsdescarted);
-
-                    }
-                    Debug.Log(volumes);
-                    Debug.Log(k);
-                    GameObject cube = CubeManaging.GetCubeinArray(k);
-                    Bounds b = cube.GetComponent<Collider>().bounds;
-                    //Bounds b = someMesh.bounds;
-                    Vector3 max = b.max;
-                    Vector3 min = b.min;
-                    Debug.Log("MAX"+max);
-                    Debug.Log("MIN"+min);
-                    Vector3 point = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
-                    while (!b.Contains(point))
-                    {
-                        point = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
-                    }
-                    //Instantiate(prefab, point, Quaternion.identity);*/
-                    Ray ray = new Ray(point, randomXY);
-                    //Ray ray = new Ray(transform.position, randomXY);
-                    //Ray ray = new Ray(transform.position, new Vector3(0, 1, 0));
-                    //Debug.DrawRay(new Vector3(0,0,0),randomXY*100,Color.green,500);
-                    RaycastHit hitInfo;
-                    //Physics.Raycast(ray, out hitInfo);
-                    /*if (Physics.Raycast(ray, out hitInfo))
-                    {
-                     total = total + 1;
-                    }*/
-                    //Part del codi per a retornar el triangle amb el que ha colisionat
-                    if (Physics.Raycast(ray, out hitInfo))
-                    {
-                        total += 1;
-                        //Physics.Raycast(ray, out hitInfo);
-                        //Debug.Log(hitInfo.triangleIndex);
-                        //MeshCollider meshCollider = hitInfo.collider as MeshCollider;
-                        /* (meshCollider == null || meshCollider.sharedMesh == null)
-                            break;*/
-
-                        /* Mesh mesh = meshCollider.sharedMesh;
-                         Vector3[] vertices = mesh.vertices;
-                         int[] triangles = mesh.triangles;
-                         Vector3 p0 = vertices[triangles[hitInfo.triangleIndex * 3 + 0]];
-                         Vector3 p1 = vertices[triangles[hitInfo.triangleIndex * 3 + 1]];
-                         Vector3 p2 = vertices[triangles[hitInfo.triangleIndex * 3 + 2]];
-                         Transform hitTransform = hitInfo.collider.transform;
-                         p0 = hitTransform.TransformPoint(p0);
-                         p1 = hitTransform.TransformPoint(p1);
-                         p2 = hitTransform.TransformPoint(p2);
-                         Debug.DrawLine(p0, p1, Color.blue, 500);
-                         Debug.DrawLine(p1, p2, Color.blue, 500);
-                         Debug.DrawLine(p2, p0, Color.blue, 500);*/
-                        if (findedTriangles[hitInfo.triangleIndex] == false)
-                        {
-                            findedTriangles[hitInfo.triangleIndex] = true;
-                            lasthitround = 1;
-                        }
-                        else
-                        {
-                            lasthitround += 1;
-                        }
-                    }
-                    else
-                    {
-                        lasthitround += 1;
-                    }
-                }
-            }
-            chrono.Stop();
-
-            Debug.Log("Total Hits " + total);
-            Debug.Log("Total Time " + chrono.ElapsedMilliseconds);
-            Debug.Log("Rounds without hit " + lasthitround);
-
-            //set new triangles
-            int painted = 0;
-            int j = 0;
-            for (int i = 0; i < nTriangles; i++) {
-                if (findedTriangles[i])
-                {
-                    painted++;
-                    nts[j] = ots[i * 3];
-                    nts[j+1] = ots[(i * 3)+1];
-                    nts[j+2] = ots[(i * 3)+2];
-                    j+= 3;
-                }
-            }
-            loadedObj.GetComponentInChildren<MeshFilter>().mesh.triangles = nts;
-            lastIndexnts = j;
-            Debug.Log("Painted Triangles = " + painted);
-            Debug.Log("Descarted Triangles = " + (nTriangles - painted));
-            Debug.Log("Of a total of " + nTriangles + " Triangles");
-
-        }
-
         if (Input.GetKey(KeyCode.R))
         {
             transform.position = originalPos;
@@ -289,6 +162,7 @@ public class CameraController : MonoBehaviour {
 
         //amb el objecte ja carregat
         ots = loadedObj.GetComponentInChildren<MeshFilter>().mesh.triangles;
+        ovs = loadedObj.GetComponentInChildren<MeshFilter>().mesh.vertices;
         nVertex = ots.Length;
         nTriangles = nVertex / 3;
         findedTriangles = new bool[nTriangles];
@@ -303,6 +177,8 @@ public class CameraController : MonoBehaviour {
     private void FindNeightbours() {
         var chrono = System.Diagnostics.Stopwatch.StartNew();
         Debug.Log("entro al Diccionari!");
+        Debug.Log("ots length" + ots.Length);
+        Debug.Log("ovs length" + ovs.Length);
         for (int i = 0; i < nVertex; i++)
         {
             if (!neighboursinfo.ContainsKey(ots[i]))
@@ -317,10 +193,32 @@ public class CameraController : MonoBehaviour {
                 neighboursinfo[ots[i]] = aux;
             }
         }
+
+        for (int p = 0; p < ovs.Length; p++)
+        {
+            if (!vertexinfo.ContainsKey(ovs[p]))
+            {
+                vertexinfo.Add(ovs[p], new List<int> { p });
+            }
+            else
+            {
+                List<int> aux = vertexinfo[ovs[p]];
+                aux.Add(p);
+                vertexinfo[ovs[p]] = aux;
+            }
+        }
         /*
         foreach (KeyValuePair<int, List<int>> kvp in neighboursinfo)
         {
-            Debug.Log("Key = {0} + Value = {1}" + kvp.Key + ": ");
+            Debug.Log("Key = " + kvp.Key + ", Values : ");
+            foreach (int i in kvp.Value)
+            {
+                Debug.Log(i);
+            }
+        }
+        foreach (KeyValuePair<Vector3, List<int>> kvp in vertexinfo)
+        {
+            Debug.Log("Key = " + kvp.Key + ", Values : ");
             foreach (int i in kvp.Value)
             {
                 Debug.Log(i);
@@ -333,10 +231,141 @@ public class CameraController : MonoBehaviour {
         Debug.Log("Final de creació del Map");
     }
 
-    private void ExpandNTimes(int n)
+    public void RayTracing(int loop) {
+
+        Debug.Log("TESTING WITH " + loop + " rays");
+        var chrono = System.Diagnostics.Stopwatch.StartNew();
+        double total = 0;
+        double lasthitround = 1;
+        double minrounds = loop * 0.1;
+        Debug.Log("minrounds : " + minrounds);
+        findedTriangles = new bool[nTriangles];
+        nts = new int[nTriangles * 3]; ;
+
+        Debug.Log(CubeManaging.GetTotalVolume());
+        //fer el loop
+        for (int i = 0; i < loop; i++)
+        {
+            if (!(lasthitround % minrounds == 0))
+            {
+                if (i % 1000 == 0) Debug.Log("Iteració : " + i);
+                //Vector3 randomXY = Random.rotation.eulerAngles;
+                Vector3 randomXY = Random.insideUnitSphere;
+                //Debug.Log(randomXY);
+                //Debug.Log(sphere.transform.position);
+                //Ray ray = new Ray(sphere.transform.position, new Vector3(randomXY.x, -1, randomXY.z));
+                float randvolume = Random.Range(0.0f, CubeManaging.GetTotalVolume());
+                ArrayList volumes = CubeManaging.GetVolumesArray();
+                Debug.Log("randV" + randvolume);
+                int k = 0;
+                float volsdescarted = 0;
+                Debug.Log("vol de " + k + ":" + volumes[k]);
+                while ((volsdescarted + (float)volumes[k]) < randvolume)
+                {
+                    Debug.Log("entro");
+                    volsdescarted += (float)volumes[k];
+                    k++;
+                    Debug.Log("vol de " + k + ":" + volumes[k]);
+                    Debug.Log("totalVuntilnow" + volsdescarted);
+
+                }
+                Debug.Log(volumes);
+                Debug.Log(k);
+                GameObject cube = CubeManaging.GetCubeinArray(k);
+                Bounds b = cube.GetComponent<Collider>().bounds;
+                //Bounds b = someMesh.bounds;
+                Vector3 max = b.max;
+                Vector3 min = b.min;
+                Debug.Log("MAX" + max);
+                Debug.Log("MIN" + min);
+                Vector3 point = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
+                while (!b.Contains(point))
+                {
+                    point = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
+                }
+                //Instantiate(prefab, point, Quaternion.identity);*/
+                Ray ray = new Ray(point, randomXY);
+                //Ray ray = new Ray(transform.position, randomXY);
+                //Ray ray = new Ray(transform.position, new Vector3(0, 1, 0));
+                //Debug.DrawRay(new Vector3(0,0,0),randomXY*100,Color.green,500);
+                RaycastHit hitInfo;
+                //Physics.Raycast(ray, out hitInfo);
+                /*if (Physics.Raycast(ray, out hitInfo))
+                {
+                 total = total + 1;
+                }*/
+                //Part del codi per a retornar el triangle amb el que ha colisionat
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    total += 1;
+                    //Physics.Raycast(ray, out hitInfo);
+                    //Debug.Log(hitInfo.triangleIndex);
+                    //MeshCollider meshCollider = hitInfo.collider as MeshCollider;
+                    /* (meshCollider == null || meshCollider.sharedMesh == null)
+                        break;*/
+
+                    /* Mesh mesh = meshCollider.sharedMesh;
+                     Vector3[] vertices = mesh.vertices;
+                     int[] triangles = mesh.triangles;
+                     Vector3 p0 = vertices[triangles[hitInfo.triangleIndex * 3 + 0]];
+                     Vector3 p1 = vertices[triangles[hitInfo.triangleIndex * 3 + 1]];
+                     Vector3 p2 = vertices[triangles[hitInfo.triangleIndex * 3 + 2]];
+                     Transform hitTransform = hitInfo.collider.transform;
+                     p0 = hitTransform.TransformPoint(p0);
+                     p1 = hitTransform.TransformPoint(p1);
+                     p2 = hitTransform.TransformPoint(p2);
+                     Debug.DrawLine(p0, p1, Color.blue, 500);
+                     Debug.DrawLine(p1, p2, Color.blue, 500);
+                     Debug.DrawLine(p2, p0, Color.blue, 500);*/
+                    if (findedTriangles[hitInfo.triangleIndex] == false)
+                    {
+                        findedTriangles[hitInfo.triangleIndex] = true;
+                        lasthitround = 1;
+                    }
+                    else
+                    {
+                        lasthitround += 1;
+                    }
+                }
+                else
+                {
+                    lasthitround += 1;
+                }
+            }
+        }
+        chrono.Stop();
+
+        Debug.Log("Total Hits " + total);
+        Debug.Log("Total Time " + chrono.ElapsedMilliseconds);
+        Debug.Log("Rounds without hit " + lasthitround);
+
+        //set new triangles
+        int painted = 0;
+        int j = 0;
+        for (int i = 0; i < nTriangles; i++)
+        {
+            if (findedTriangles[i])
+            {
+                painted++;
+                nts[j] = ots[i * 3];
+                nts[j + 1] = ots[(i * 3) + 1];
+                nts[j + 2] = ots[(i * 3) + 2];
+                j += 3;
+            }
+        }
+        loadedObj.GetComponentInChildren<MeshFilter>().mesh.triangles = nts;
+        Debug.Log("Painted Triangles = " + painted);
+        Debug.Log("Descarted Triangles = " + (nTriangles - painted));
+        Debug.Log("Of a total of " + nTriangles + " Triangles");
+
+    }
+
+
+    public void ExpandNTimes(int n)
     {
         for (int i = 0; i < n; i++)
         {
+            Debug.Log("FAIG EXPANSIO");
             DoExpansion();
         }
     }
@@ -344,36 +373,66 @@ public class CameraController : MonoBehaviour {
     private void DoExpansion()
     {
         //tenint els nous triangles visibles des del punt, fem visibles tots els seus veins (comparteixen un vertex)
-        int[] auxnts = nts;
-        bool[] auxfinded = findedTriangles;
-        int l = lastIndexnts;
+        int[] auxnts = (int[])nts.Clone();
+        bool[] auxfinded = (bool[])findedTriangles.Clone();
+        /*Debug.Log("INICIAL");
+        for (int  b= 0; b < auxfinded.Length;  ++b)
+        {
+            Debug.Log(b+" : "+auxfinded[b]);
+        }*/
         for (int i = 0; i < nTriangles; i++)
         {
             if (findedTriangles[i])
             {
-                auxfinded[i] = true;
                 //mirar els vehins dels 3 vertex
-                for (int j = 0; j < 3; ++j)
+                /*Debug.Log("Triangle : " + i);
+                Debug.Log("Auxfinded : " + auxfinded[i]);
+                Debug.Log("findedTriangles : " + findedTriangles[i]);
+                Debug.Log("Miro els vertex : " + i * 3 + ", " + ((i * 3) + 1) + "," + ((i * 3) + 2));*/
+                for (int y = 0; y < 3; ++y)
                 {
-                    int v = ots[(i * 3) + j];
-                    List<int> neighb = neighboursinfo[v];
-                    for (int k = 0; k < neighb.Count; ++k) {
-                        if (!findedTriangles[k])
+                    
+                    int v = ots[(i * 3) + y];
+                    //Debug.Log("For de veï numero : " + y + ", que es el triangle:" + ovs[v]);
+                    List<int> idemVertex = vertexinfo[ovs[v]];
+
+                    foreach(int x in idemVertex)
+                    {
+                        //Debug.Log("Miro primer el vertex : " + x);
+                        List<int> triangleNeighb = neighboursinfo[x];
+                        
+                        foreach(int k in triangleNeighb)
                         {
-                            auxfinded[k] = true;
-                            auxnts[l] = ots[i * 3];
-                            auxnts[l + 1] = ots[(i * 3) + 1];
-                            auxnts[l + 2] = ots[(i * 3) + 2];
-                            l += 3;
+                            //Debug.Log("i veig els veï : " + k);
+                            if (!findedTriangles[k])
+                            {
+
+                                auxfinded[k] = true;
+                            }
                         }
                     }
                 }
-
+                /*Debug.Log("Depres d'iterar");
+                for (int b = 0; b < auxfinded.Length; ++b)
+                {
+                    Debug.Log(b + " : " + auxfinded[b]);
+                }*/
             }
         }
-        findedTriangles = auxfinded;
-        nts = auxnts;
-        lastIndexnts = l;
-        loadedObj.GetComponentInChildren<MeshFilter>().mesh.triangles = nts;
+
+        int j = 0;
+        for (int i = 0; i < nTriangles; i++)
+        {
+            if (auxfinded[i])
+            {
+                auxnts[j] = ots[i * 3];
+                auxnts[j + 1] = ots[(i * 3) + 1];
+                auxnts[j + 2] = ots[(i * 3) + 2];
+                j += 3;
+            }
+        }
+        findedTriangles = (bool[])auxfinded.Clone();
+        nts = (int[])auxnts.Clone();
+        loadedObj.GetComponentInChildren<MeshFilter>().mesh.triangles = (int[])nts.Clone();
     }
 }
